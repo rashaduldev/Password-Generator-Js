64a742e7f393a697517c17a4f88ab6cd2e2ef7ee
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-04-25 16:46:51 +0600

    All Done

--- password.js
+++ password.js
@@ -6,6 +6,7 @@ let uppercase = document.getElementById("uppercase");
 let numbers = document.getElementById("numbers");
 let symbols = document.getElementById("symbols");
 let genbtn = document.getElementById("genbtn");
+let copyicon = document.getElementById("copyicon");
 
 // Showing input slider value
 slidervalue.textContent=inputslider.value;
@@ -17,13 +18,49 @@ inputslider.addEventListener('input', ()=>{
 genbtn.addEventListener('click',()=>{
     passbox.value=generatePassword();
 })
+let lowerChars = "abcdefghijklmnopqrstuvwxyz";
+let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let allNumbers = "0123456789";
+let allSymbols = "~!@#$%^&*"; 
 // Function to generate password
 function generatePassword(){
-    let genbtn="";
-    genbtn = math.floor(math.random());
-    return genbtn;
+    let genPassword="";
+    let allChars = "";
+
+    allChars  += lowercase.checked ? lowerChars : "";
+    allChars  += uppercase.checked ? upperChars : "";
+    allChars  += numbers.checked ? allNumbers : "";
+    allChars  += symbols.checked ? allSymbols : "";
+
+    if(allChars == "" || allChars.length == 0){
+        return genPassword;
+    }
+    
+
+    let i = 1;
+    while(i<=inputslider.value){
+        genPassword += allChars.charAt(Math.floor(Math.random() * allChars.length));
+        i++;
+    }
+
+
+    // genPassword =allChars.charAt(Math.floor(Math.random()*allChars.length));
+    // generatePassword =lowerChars.charAt(Math.floor(Math.random()*lowerChars.length));
+    return genPassword;
 }
 
+copyicon.addEventListener('click',()=>{
+    if(passbox.value !="" || passbox.value.length >=1){
+        navigator.clipboard.writeText(passbox.value);
+        copyicon.innerText="check";
+        copyicon.title="password.copied";
+        setTimeout(()=>{
+            copyicon.innerHTML = "content_copy";
+            copyicon.title = "";
+        }, 2000)
+    }
+
+}); 
 
 
 
