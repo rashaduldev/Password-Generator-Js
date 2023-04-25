93510b62e2d032c054a5f67480beac20cd67ee20
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-04-25 14:12:46 +0600

    There was an error

--- password.js
+++ password.js
@@ -1,9 +1,27 @@
 let inputslider = document.getElementById("inputslider");
 let slidervalue = document.getElementById("slidervalue");
+let passbox = document.getElementById("passbox");
+let lowercase = document.getElementById("lowercase");
+let uppercase = document.getElementById("uppercase");
+let numbers = document.getElementById("numbers");
+let symbols = document.getElementById("symbols");
+let genbtn = document.getElementById("genbtn");
 
+// Showing input slider value
 slidervalue.textContent=inputslider.value;
-
-
+inputslider.addEventListener('input', ()=>{
+    slidervalue.textContent=inputslider.value;
+})    
+// slider value end
+genbtn.addEventListener('click',()=>{
+    passbox.value=generatePassword();
+})
+// Function to generate password
+function generatePassword(){
+    let genPassword="";
+    genPassword = math.random() *2;
+    return genPassword;
+}
 
 
 
